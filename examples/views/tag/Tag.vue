1bc8d244d7610f709fc80f7e91a0d4f18fb2b8a5
qixiuqx <qingwu13950520@163.com>
2021-09-15 02:58:21 +0800

    优化展示样式显示效果

---
+++ examples/views/tag/Tag.vue
@@ -0,0 +1,53 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Seven
+ * @Date: 2021-09-15 02:33:06
+ * @LastEditors: Seven
+ * @LastEditTime: 2021-09-15 02:56:48
+-->
+<template>
+  <div class="">
+    <XTag>默认 标签</XTag>
+    <XTag style="margin-left:10px" type="primary">主要 标签</XTag>
+    <XTag style="margin-left:10px" type="success">成功 标签</XTag>
+    <XTag style="margin-left:10px" type="info">信息 标签</XTag>
+    <XTag style="margin-left:10px" type="warning">警告 标签</XTag>
+    <XTag style="margin-left:10px" type="error">错误 标签</XTag>
+
+    <pre v-text="str"></pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      str: `
+    <XTag>默认 标签</XTag>
+    <XTag style="margin-left:10px" type="primary">主要 标签</XTag>
+    <XTag style="margin-left:10px" type="success">成功 标签</XTag>
+    <XTag style="margin-left:10px" type="info">信息 标签</XTag>
+    <XTag style="margin-left:10px" type="warning">警告 标签</XTag>
+    <XTag style="margin-left:10px" type="error">错误 标签</XTag>
+          
+
+    type: {
+      type: String,
+      default: "default",
+    },
+    methods: {
+        handleTagClick() {
+        this.$emit("click");
+        },
+    },          
+          
+          
+          `,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ packages/tag/Tag.vue
@@ -0,0 +1,79 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Seven
+ * @Date: 2021-09-15 02:24:15
+ * @LastEditors: Seven
+ * @LastEditTime: 2021-09-15 02:53:22
+-->
+<template>
+  <span :class="`x-tag x-tag-${type}`" @click="handleTagClick">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "XTag",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+  },
+  methods: {
+    handleTagClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.x-tag {
+  display: inline-block;
+  padding: 0 10px;
+  height: 32px;
+
+  color: #fff;
+  line-height: 30px;
+  border-radius: 3px;
+  white-space: nowrap;
+  cursor: pointer;
+}
+.x-tag-default {
+  border: 1px solid #dcdfe6;
+  background-color: #ffffff;
+  color: #333;
+}
+.x-tag-primary {
+  background-color: #409eff;
+  &:hover {
+    background-color: #66b1ff;
+  }
+}
+.x-tag-success {
+  background-color: #67c23a;
+  &:hover {
+    background-color: #85ce61;
+  }
+}
+.x-tag-info {
+  background-color: #909399;
+  &:hover {
+    background-color: #a6a9ad;
+  }
+}
+.x-tag-warning {
+  background-color: #e6a23c;
+  &:hover {
+    background-color: #ebb563;
+  }
+}
+.x-tag-error {
+  background-color: #f56c6c;
+  &:hover {
+    background-color: #f78989;
+  }
+}
+</style>
