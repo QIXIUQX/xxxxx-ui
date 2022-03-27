f8b5d550c2a2851bf3faba37053704f8cc7d8342
QIXIUQX <qingwu13950520@outlook.com>
2022-03-27 11:09:22 +0800

    增加添加到body组件,用于图片在固定定位时候会抖动问题,

---
+++ packages/in-body/InBody.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="">
+		<slot></slot>
+	
+	</div>
+</template>
+
+<script>
+/** 组件/工具类/store 导入 */
+
+/** 网络请求导入 导入 */
+
+export default {
+	name: "",
+	methods: {
+		/** 事件或其他相关方法 **/
+		name() {
+		
+		}
+		/** 网络请求相关方法 **/
+	},
+	mounted() {
+		const node = this.$mount().$el; //
+		document.body.appendChild(node)
+	},
+	beforeDestroy() {
+		const node = this.$mount().$el;
+		node.remove()
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
